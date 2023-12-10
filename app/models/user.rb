29d5a90ef49aa7ccd4b254c37e79367bdb18e44d
Tushar Kulkarni <tskulkarni25@gmail.com>
2023-12-10 18:19:30 -0500

    real-time password feedback app

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]+\z/, message: 'must include lowercase, uppercase, digit, and special character' }
+end
